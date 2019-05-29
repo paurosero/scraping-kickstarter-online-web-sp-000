c924fc75f3187bcb6bf3f648a372cb675880674b
Paul Rosero <paul_rosero@yahoo.com>
2019-05-29 20:36:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,10 +7,9 @@ require 'pry'
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
     
-    
-    
     kickstaer.css("li.project.grid_4").each do |project|
-      projects[project] = {}
+     title = prokect/css("h2.bbcard_name strng a").text
+     projects[title.to_sym] = {}
     end
     projects
   end
