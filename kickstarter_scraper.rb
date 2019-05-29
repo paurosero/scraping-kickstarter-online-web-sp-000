893f666f519c5dbe4e059cfc2e16702c84b70963
Paul Rosero <paul_rosero@yahoo.com>
2019-05-29 20:30:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,8 @@
-html = File.read('fixtures/kickstarter.html')
 
-kickstarter = Nokogiri::HTML(html)
 
 require 'nokogiri'
 require 'pry'
 
   def create_project_hash
-    # write your code here
+    
   end
